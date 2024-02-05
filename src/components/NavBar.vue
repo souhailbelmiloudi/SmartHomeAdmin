db7e3fe4ffecad5b3ded1a96763b4e7f2ad8407a
souhail belmiloudi <souhailbelmiloudi@gmail.com>
2024-02-05 21:58:49 +0100

    final

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,13 @@
 <template>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">ADMIN</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+     <RouterLink to="/" class="navbar-brand">
+        Admin panel
+     </RouterLink>
+     
+ <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
